eadd9106246e7d35750a8d6211e86d13aed2987e
Dự Nguyễn Chiếm <dunguyenchiem@Dus-MacBook-Pro.local>
2023-05-16 10:53:34 +0700

    feature: pose detection

--- lib/image_segmentation.dart
+++ lib/image_segmentation.dart
@@ -2,12 +2,14 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 import 'package:google_mlkit_selfie_segmentation/google_mlkit_selfie_segmentation.dart';
 import 'package:grwm_flutter_ai/painters/segmentation_painter.dart';
 import 'package:image_size_getter/file_input.dart';
 import 'package:image_size_getter/image_size_getter.dart' hide Size;
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
+import 'painters/pose_painter.dart';
 
 Future<File> createFileFromUrl(String imageUrl) async {
   // Get the temporary directory of the device.
@@ -29,28 +31,38 @@ Future<File> createFileFromUrl(String imageUrl) async {
 }
 
 Future imageSegmentation(File file) async {
-  // String url =
-  //     'https://img.freepik.com/premium-photo/man-with-sunglasses-taking-selfie_23-2148666530.jpg';
-  // final File file = await createFileFromUrl(url);
-
   final InputImage inputImage = InputImage.fromFile(file);
 
   final segmenter = SelfieSegmenter(
     mode: SegmenterMode.stream,
-    // enableRawSizeMask: true,
   );
-  final mask = await segmenter.processImage(inputImage);
+  var mask = await segmenter.processImage(inputImage);
   final size = ImageSizeGetter.getSize(FileInput(file));
   final painter = SegmentationPainter(
     mask!,
     Size(size.width.toDouble(), size.height.toDouble()),
     InputImageRotation.rotation0deg,
   );
-  // var customPaint = CustomPaint(painter: painter);
   customPaintStreamController.add(painter);
   segmenter.close();
+}
 
-  // return customPaint;
+Future imagePoseDetection(File file) async {
+  final InputImage inputImage = InputImage.fromFile(file);
+
+  final PoseDetector _poseDetector =
+      PoseDetector(options: PoseDetectorOptions());
+  final poses = await _poseDetector.processImage(inputImage);
+
+  final size = ImageSizeGetter.getSize(FileInput(file));
+  final painter = PosePainter(
+      poses,
+      Size(size.width.toDouble(), size.height.toDouble()),
+      size.needRotate
+          ? InputImageRotation.rotation90deg
+          : InputImageRotation.rotation0deg);
+  customPaintStreamController.add(painter);
+  _poseDetector.close();
 }
 
 StreamController<CustomPainter> customPaintStreamController =
